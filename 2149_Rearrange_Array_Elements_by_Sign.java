d79e395067b1177c0985988affd9f1d2d200508e
Chaitanya Kulkarni <144622122+ChaitanyaKulkarni001@users.noreply.github.com>
2025-01-14 10:39:50 +0530

    Update 2149_Rearrange_Array_Elements_by_Sign

--- 2149_Rearrange_Array_Elements_by_Sign.java
+++ 2149_Rearrange_Array_Elements_by_Sign.java
@@ -1,6 +1,23 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        
+        // Optimal Approach 
+
+        int pos=0;
+        int neg=1;
+        int[] arr = new int[nums.length];
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]>0){
+                arr[pos] = nums[i];
+                pos= pos+2;
+            }
+            else{
+                arr[neg] = nums[i];
+                neg=neg+2;
+            }
+        }
+        return arr;
+
+        // Brute Force Approach        
         int len = nums.length;
         int[] pos = new int[len/2];
         int[] neg = new int[len/2];
@@ -33,6 +50,9 @@ class Solution {
 
         }
         return nums;
+
+
+        
 
 
 
